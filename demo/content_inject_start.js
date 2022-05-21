2b9c98249c91ae6d10466eb5e9fff5aaa5bfee8a
ahuigo <1781999+ahuigo@users.noreply.github.com>
2022-05-21 11:13:52 +0800

    initUAA

--- demo/content_inject_start.js
+++ demo/content_inject_start.js
@@ -1,8 +1,12 @@
 // called by document_start.js
-if (window.location.hostname=='m'){
+function initUA(){
     Object.defineProperty(navigator, 'platform', {
         get: function(){
             return "Android"
+    }});
+    Object.defineProperty(navigator, 'userAgent', {
+        get: function(){
+            return "User-Agent: Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36"
     }});
 }
 
@@ -27,9 +31,8 @@ function cleanZhihu(){
     switch(location.hostname){
         case 'www.zhihu.com':
         case 'zhihu.com': cleanZhihu();break;
+        case 'm2': initUA();break;
     }
     console.log(location.href)
 })()
-
-
 console.log("new navigator.platform:", navigator.platform)
