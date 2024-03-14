9844280c7693cae87975b09bc7c5392535ae84f7
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-03-14 15:47:48 +0800

    cookie

--- chrome-ahui/document_start.js
+++ chrome-ahui/document_start.js
@@ -1,21 +1,22 @@
 /**
  * Content scripts are executed in an "isolated world" environment.
  * This example will show you how to inject js to web page when `document_start`.
- * See https://stackoverflow.com/questions/9515704/use-a-content-script-to-access-the-page-context-variables-and-functions/9517879#9517879 
-  */
+ * See https://stackoverflow.com/questions/9515704/use-a-content-script-to-access-the-page-context-variables-and-functions/9517879#9517879
+ */
 function injectScript(src) {
-    const s = document.createElement('script');
-    s.src = chrome.runtime.getURL(src);
-    s.type = "module"
-    s.onload = function() {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
+  const s = document.createElement("script");
+  s.src = chrome.runtime.getURL(src);
+  s.type = "module";
+  s.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
 }
 
-injectScript('inject/ajax.js')
-injectScript('inject/main.js')
+injectScript("inject/ajax.js");
+injectScript("inject/main.js");
+injectScript("inject/cookie.js");
 
 // 1. This window.ht1 could be read by document_end.js
 // 2. But window.ht1 is isolated with tab's window.
-Window.prototype.ht1 = 'ht1';
+Window.prototype.ht1 = "ht1";
--- chrome-ahui/inject/cookie.js
+++ chrome-ahui/inject/cookie.js
@@ -1,19 +1,20 @@
 // cookie proxy ///////////////////////////////////////
-function proxyCookie(){
-    const cookieDesc = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie') ||
-                 Object.getOwnPropertyDescriptor(HTMLDocument.prototype, 'cookie');
-    if (cookieDesc && cookieDesc.configurable) {
-        Object.defineProperty(document, 'cookie', {
-            get: function () {
-                return cookieDesc.get.call(document);
-            },
-            set: function (val) {
-                console.log("set cookie",val);
-                //debugger;
-                //alert('set cookie:'+val)
-                cookieDesc.set.call(document, val);
-            }
-        });
-    }
+function proxyCookie() {
+  const cookieDesc =
+    Object.getOwnPropertyDescriptor(Document.prototype, "cookie") ||
+    Object.getOwnPropertyDescriptor(HTMLDocument.prototype, "cookie");
+  if (cookieDesc && cookieDesc.configurable) {
+    Object.defineProperty(document, "cookie", {
+      get: function () {
+        return cookieDesc.get.call(document);
+      },
+      set: function (val) {
+        console.log("set cookie", val);
+        //debugger;
+        //alert('set cookie:'+val)
+        cookieDesc.set.call(document, val);
+      },
+    });
+  }
 }
-proxyCookie()
+// proxyCookie();
